c72489782031b69252f961c5cdad880e140a3d68
Maytton <maytton0000@gmail.com>
2025-03-06 10:31:16 -0300

    config do main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:game/screens/game_screen.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const GameScreen(),
+    );
+  }
 }
 
 
